b6a1cc9cef8586690f3d9164b6619c998f10aa15
stmtk <13043069@gakushuin.ac.jp>
2017-06-02 15:03:47 +0900

    index.json追加

---
+++ script.js
@@ -0,0 +1,7 @@
+const req = await fetch("index.json");
+const data = await req.json();
+document.body.innerHTML += "<ul>";
+for(const item of data){
+    document.body.innerHTML += `<li>${item}</li>`;
+}
+document.body.innerHTML += "</ul>";
