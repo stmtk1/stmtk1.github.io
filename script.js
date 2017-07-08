1dfb20e8988392e470cbc4a2b845b19678cbe158
stmtk <13043069@gakushuin.ac.jp>
2017-07-08 23:32:58 +0900

    星空風にした

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 (async ()=>{
     const req = await fetch("index.json");
     const data = await req.json();
@@ -6,5 +7,28 @@
         body += `<li><a href="${item}">${item}</a></li>`;
     }
     body += "</ul>";
-    document.body.innerHTML += body;
+    document.getElementById("container").innerHTML += body;
 })();
+
+var color = ()=>{
+    var blue = Math.floor(Math.random() * 0x100).toString(16);
+    if(blue.length != 2) blue = "0" + blue;
+    var r_g = Math.floor(Math.random() * 0x80 + 0x80).toString(16);
+    if(r_g.length != 2) r_g = "0" + r_g;
+    console.log("#ffff" + blue);
+    return "#" + r_g + r_g + blue;
+};
+var canvas = document.getElementById("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var ctx = canvas.getContext("2d");
+
+for(var i = 0; i < 100; i++){
+    ctx.fillStyle = color();
+    ctx.beginPath();
+    var x = Math.random() * canvas.width;
+    var y = Math.random() * canvas.height;
+    var r = Math.random() + canvas.width / 600;
+    ctx.arc(x, y, r, 0, 2 * Math.PI);
+    ctx.fill();
+}
